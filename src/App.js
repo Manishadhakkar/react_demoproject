4d162602acf08b6fb56b4ca7522f40c3bd305e20
manishadhakkar <107385021+Manishadhakkar@users.noreply.github.com>
2024-05-13 12:36:00 +0530

    Update5 App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload santanu sahoo.
+          Edit <code>src/App.js</code> and save to reload basant kapri.
         </p>
         <a
           className="App-link"
